e0fefa580f52610ebdc1bdbecf15ddc53df0065d
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-03-19 02:39:46 +0700

    Init

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,6 +1,21 @@
+// import { Module } from "@nestjs/common";
+// import { AnalyticsController } from "./analytics.controller";
+// import { AnalyticsService } from "./analytics.service";
+
+// @Module({ controllers: [AnalyticsController], providers: [AnalyticsService] })
+// export class AnalyticsModule {}
 import { Module } from "@nestjs/common";
+import { BullModule } from "@nestjs/bullmq";
 import { AnalyticsController } from "./analytics.controller";
 import { AnalyticsService } from "./analytics.service";
+import { QueueNames } from "../../common/constants/queue-names.constant";
 
-@Module({ controllers: [AnalyticsController], providers: [AnalyticsService] })
+@Module({
+  imports: [
+    BullModule.registerQueue({ name: QueueNames.BROADCAST }),
+    BullModule.registerQueue({ name: QueueNames.WEBHOOK }),
+  ],
+  controllers: [AnalyticsController],
+  providers: [AnalyticsService],
+})
 export class AnalyticsModule {}
--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,8 +1,19 @@
+// import { Module } from "@nestjs/common";
+// import { NotificationsService } from "./notifications.service";
+// import { EmailService } from "./email.service";
+
+// @Module({
+//   providers: [NotificationsService, EmailService],
+//   exports: [NotificationsService],
+// })
+// export class NotificationsModule {}
 import { Module } from "@nestjs/common";
 import { NotificationsService } from "./notifications.service";
 import { EmailService } from "./email.service";
+import { GatewayModule } from "../../gateway/gateway.module";
 
 @Module({
+  imports: [GatewayModule],
   providers: [NotificationsService, EmailService],
   exports: [NotificationsService],
 })
